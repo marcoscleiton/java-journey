7a84b374cdaf3fb5cd677c5c9cd4da75ab67240e
marcoscleiton <marcosfeitosa54@gmail.com>
2025-12-19 18:03:44 -0300

    feat: adiciona programa par ou ímpar

---
+++ 01-fundamentos/ParOuImpar.java
@@ -0,0 +1,11 @@
+public class ParOuImpar {
+    public static void main(String[] args) {
+        int numero = 8;
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é par");
+        } else {
+            System.out.println("O número é ímpar");
+        }
+    }
+}
