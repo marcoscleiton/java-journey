2622ff93bbc64b76338cf8570ff30f751c7c939d
marcoscleiton <marcosfeitosa54@gmail.com>
2026-01-02 16:35:57 -0300

    Add example of comparative expressions in Java

---
+++ 01-fundamentos/expressoes-omparativas.java
@@ -0,0 +1,18 @@
+public class ExpressoesComparativas {
+
+    public static void main(String[] args) {
+
+        int pao = 10;
+        int acucar = 20;
+        String titulo = "=== EXPRESSÕES COMPARATIVAS ===";
+
+        System.out.println(titulo);
+
+        System.out.println("pao == acucar: " + (pao == acucar));
+        System.out.println("pao != acucar: " + (pao != acucar));
+        System.out.println("pao > acucar: " + (pao > acucar));
+        System.out.println("pao < acucar: " + (pao < acucar));
+        System.out.println("pao >= acucar: " + (pao >= acucar));
+        System.out.println("pao <= acucar: " + (pao <= acucar));
+    }
+}
