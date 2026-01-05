43ba3bf6e2d19166ddb9e26307e90dd87c15ef3d
marcoscleiton <marcosfeitosa54@gmail.com>
2026-01-05 15:50:25 -0300

    estrutura-if-else-java
    Examples of if/else structure and ternary operator in Java.

--- 01-fundamentos/EstruturaIfElse.java
+++ 01-fundamentos/EstruturaIfElse.java
@@ -1,18 +1,49 @@
 public class EstruturaIfElse {
-
     public static void main(String[] args) {
 
+        //  int idade = 15;
+
+        //  if (idade >= 18) {
+        //      System.out.println("Você é maior de idade");
+        //  }
+        //  else {
+        //      System.out.println("Você é menor de idade");
+        //  }
+        // boolean estaChovendo = true;
+        // if (!estaChovendo) {
+        //     System.out.println("Vamos sair para passear");
+            
+        // }
+        // else {
+        //     System.out.println("Vamos ficar em casa");
         boolean temDinheiro = true;
         boolean temCartao = false;
 
         if (temDinheiro && temCartao) {
-            System.out.println("Pede um iFood e um Zé Delivery");
+            System.out.println("Pede um ifood e um zé delivery");
 
-        } else if (temDinheiro || temCartao) {
-            System.out.println("Pede um iFood");
-
-        } else {
-            System.out.println("Não pede nada");
         }
-    }
+        else if (temDinheiro || temCartao) {
+            System.out.println("Pede um ifood");
+            
+        }
+        else {
+            System.out.println("Não pede nada");
+
+
+
+        }
+        // ============================
+        // OPERADOR TERNÁRIO
+        // ============================
+        System.out.println("\n=== OPERADOR TERNÁRIO ===");
+
+        String mensagem = temDinheiro
+                ? "Pede um iFood"
+                : "Come o que tem em casa";
+
+        System.out.println(mensagem);
+       
+        }
+       
 }
